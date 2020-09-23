d7c3b30e8b9dd2da3eaefed639b37f8e991065f7
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2020-09-23 14:33:33 -0500

    Post model serialized and fields tested ok

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,13 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :title, :content, :published, :author
+
+  def author
+    user = self.object.user
+    {
+      email: user.email,
+      name: user.name,
+      id: user.id
+
+    }
+  end
 end

a7d6ce639150b47093f9f929f352f6d3fb46a4e5
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2020-09-24 12:57:59 -0500

    Logic of authorize user moved to concern 'secured'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+    include Secured
     before_action :authenticate_user!, only: [:create, :update]
 
     rescue_from Exception do |e|
@@ -55,19 +56,4 @@ class PostsController < ApplicationController
         params.require(:post).permit(:title, :content, :published)
     end
 
-    def authenticate_user!
-        #Bearer xxxxx
-        token_regex = /Bearer (\w+)/
-        #Leer Header de auth
-        headers = request.headers
-        #Verificar que sea valido
-        if headers['Authorization'].present? && headers['Authorization'].match(token_regex)
-            token = headers['Authorization'].match(token_regex)[1]
-
-            if (Current.user = User.find_by_auth_token(token))
-                return
-            end
-        end
-        render json: { error: 'Unauthorized' }, status: :unauthorized
-    end
 end
